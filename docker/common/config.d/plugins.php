c5fd295436c62cb40b109a8ce7197f3dd2e1f399
Victor Ferreira <vitfera@hotmail.com>
2025-04-23 16:07:16 -0300

    install plugin RecreatePCacheOnLogin

--- docker/common/config.d/plugins.php
+++ docker/common/config.d/plugins.php
@@ -5,5 +5,6 @@ return [
         'MultipleLocalAuth' => [ 'namespace' => 'MultipleLocalAuth' ],
         'SamplePlugin' => ['namespace' => 'SamplePlugin'],
         'Accessibility' => ['namespace' => 'Accessibility'],
+        'RecreatePCacheOnLogin' => ['namespace' => 'RecreatePCacheOnLogin'],
     ]
 ];
